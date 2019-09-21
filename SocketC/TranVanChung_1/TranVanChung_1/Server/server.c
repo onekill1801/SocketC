63b32089dac79a4f3a9b1c01f071edd033fae305
chungtv <onekill1818@gmail.com>
2019-09-21 10:07:52 +0700

    cleancode continue

---
+++ SocketC/TranVanChung_1/TranVanChung_1/Server/server.c
@@ -0,0 +1,52 @@
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#define MAX 80 
+#define PORT 8888
+#define SA struct sockaddr 
+
+
+int main() 
+{ 
+	int sockfd, connfd, len; 
+	struct sockaddr_in servaddr, cli; 
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	servaddr.sin_port = htons(PORT); 
+	int y = bind(sockfd, (SA*)&servaddr, sizeof(servaddr));
+	int x = listen(sockfd, 5);
+	len = sizeof(cli); 
+	connfd = accept(sockfd, (SA*)&cli, &len); 
+	char buff[MAX]; 
+
+	printf("Client's IP: %s\n", inet_ntoa(cli.sin_addr));
+    printf("Client's port: %d\n", ntohs(cli.sin_port));
+
+	read(connfd, buff, sizeof(buff));
+	printf("%s\n", buff); 
+	bzero(buff, MAX);
+
+	char temp[] = "Hello Client\nWhat is your name? ";
+	strcpy(buff,temp);
+	bzero(temp, sizeof(temp));
+	write(connfd, buff, sizeof(buff)); 
+
+	read(connfd, buff, sizeof(buff));
+	printf("My name is %s!Nice to meet you!\n", buff); 
+	bzero(buff, MAX);
+
+	read(connfd, buff, sizeof(buff));
+	printf("%s\n", buff); 
+	bzero(buff, MAX);
+
+	close(sockfd); 
+} 
